83dac82c76abbb1f6732f2b09bf64a4aee6a263e
David Ceresuela <davidzgz123@gmail.com>
2012-02-24 21:44:10 +0100

    First commit: README + composition

---
+++ composition/vehicle.rb
@@ -0,0 +1,35 @@
+#Vehicle
+#------------------
+#   start_engine()
+#   stop_engine()
+#------------------
+#       /\
+#       ||
+#------------------
+#Car
+#------------------
+
+
+class Vehicle
+
+   # All sorts of vehicle-related code...
+   
+   def start_engine
+      #Start the engine
+   end
+   
+   def stop_engine
+      # Stop the engine
+   end
+   
+end
+
+class Car < Vehicle
+
+   def sunday_drive
+      start_engine
+      # Cruise out into the country and return
+      stop_engine
+   end
+
+end
